a0e445f1be959361697838b50ae6cdfce1d82234
xiaofengfu <fuxiaofeng1107@163.com>
2017-12-29 19:13:30 +0800

    mysql table column limit long

--- mysql.sql
+++ mysql.sql
@@ -5,8 +5,8 @@ CREATE TABLE `action_queue` (
   `action_status` tinyint(4) DEFAULT '1' COMMENT '{1:未执行,0:执行失败,2:执行成功}',
   `fail_num` tinyint(4) DEFAULT '0' COMMENT '总的失败次数',
   `try_num` tinyint(4) DEFAULT '0' COMMENT '失败重试次数',
-  `action_str` varchar(500) DEFAULT NULL COMMENT 'action',
-  `params` varchar(500) DEFAULT NULL COMMENT '参数',
+  `action_str` varchar(1024) DEFAULT NULL COMMENT 'action',
+  `params` varchar(1024) DEFAULT NULL COMMENT '参数',
   `ip` varchar(255) DEFAULT NULL COMMENT '执行任务ip',
   `create_time` datetime DEFAULT NULL COMMENT '任务创建时间',
   `finish_time` datetime DEFAULT NULL COMMENT '任务执行结束时间',
@@ -16,10 +16,10 @@ CREATE TABLE `action_queue` (
 DROP TABLE IF EXISTS web_url_table;
 CREATE TABLE `web_url_table` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `url` varchar(255) DEFAULT NULL COMMENT 'url链接',
+  `url` varchar(1024) DEFAULT NULL COMMENT 'url链接',
   `title` VARCHAR(255) DEFAULT NULL COMMENT '当前url的title',
   `content_type` VARCHAR(255) DEFAULT NULL COMMENT '当前url的content_type',
-  `referer` VARCHAR(255) DEFAULT NULL COMMENT 'url网站来源',
+  `referer` VARCHAR(1024) DEFAULT NULL COMMENT 'url网站来源',
   `hostname` varchar(50) DEFAULT NULL COMMENT 'url的host',
   `params` varchar(500) DEFAULT NULL COMMENT '参数',
   `md5` varchar(50) NOT NULL UNIQUE COMMENT 'url md5值',
